35b16927ca7b721015e16b74960ebc64b174b869
zhongkouwei <726442240@qq.com>
2017-05-26 15:29:42 +0800

    登陆部分

--- App/Admin/Controller/CommonController.class.php
+++ App/Admin/Controller/CommonController.class.php
@@ -11,9 +11,12 @@ use Think\Controller;
 
 class CommonController extends Controller
 {
-    // 是否登陆状态
+    public $input;
+
+    // 构造函数
     function _initialize()
     {
-
+        import('ORG.Util.Input');
+        $this->input = \Input::getInstance();
     }
 }
--- App/Admin/Controller/LoginController.class.php
+++ App/Admin/Controller/LoginController.class.php
@@ -8,27 +8,51 @@
 
 namespace Admin\Controller;
 use Think\Controller;
+use Think\Image;
 
 class LoginController extends Controller
 {
     public function login()
     {
-
+        layout(false);
+        $this->display();
     }
 
     // 验证码
     public function verify()
     {
-
+        import('ORG.Util.Image');
+        \Image::buildImageVerify(4, 5, 'png', 55, 28);
     }
 
     public function checkLogin()
     {
+        $input = \Input::getInstance();
+        $username = $input->getVar($_POST['username']);
+        $password = md5($input->getVar($_POST['password']));
+        $verify = md5($input->getVar($_POST['password']));
+
+        if($verify != $_SESSION['verify']) {
+            $this->error('验证码错误', U('Login/login'));
+        }
+        $user = M('user');
+        $re =  $user->field('userId, userPassword')->where('userName='.$username)->find();
+        if (empty($re)){
+            $this->error('没有此用户', U('Login/login'));
+        }
+        if($password != $re['userPassword']){
+            $this->error('密码错误', U('Login/login'));
+        }
+
+        $_SESSION['id'] = $re['userId'];
+        $_SESSION['userName'] = $re['userName'];
+        $this->redirect('Index/index');
 
     }
 
     public function logout()
     {
-
+        session_destroy();
+        $this->redirect('Login/login');
     }
 }
