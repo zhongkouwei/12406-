1d7acc8b0bb0637029f4aeb041fddce769f49ec4
zhongkouwei <726442240@qq.com>
2017-06-12 11:40:12 +0800

    订单展示

--- App/Admin/Controller/OrderController.class.php
+++ App/Admin/Controller/OrderController.class.php
@@ -8,12 +8,22 @@
 
 namespace Admin\Controller;
 
+use Admin\Model\OrderModel;
+
 class OrderController extends CommonController
 {
+    private $_client;
+    public function _initialize()
+    {
+        $this->_client = new OrderModel();
+    }
+
     // 订单列表
     public function ordersList()
     {
-
+        $data = $this->_client->getOrderList();
+        $this->assign('data', $data);
+        $this->display();
     }
 
 }
